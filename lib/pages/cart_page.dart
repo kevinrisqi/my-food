d22978128e2cf611b770543aadd786dc884c158b
kevin <kevinrisqi22@gmail.com>
2021-07-12 19:03:08 +0700

    Update Cart Page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -8,6 +8,9 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  int qty = 1;
+  bool isChecked = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,12 +44,21 @@ class _CartPageState extends State<CartPage> {
               ),
             ),
             Container(
+              padding: EdgeInsets.all(8),
               margin: EdgeInsets.symmetric(horizontal: defaultMargin),
-              child: Column(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Row(
                 children: [
+                  Container(
+                    child: Icon(Icons.check),
+                  ),
                   Image.asset(
-                    width: 100,
-                    height: 100,
+                    "assets/img/hamburger.png",
+                    width: 60,
+                    height: 60,
                   ),
                   SizedBox(
                     width: 14,
@@ -55,15 +67,84 @@ class _CartPageState extends State<CartPage> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text('Hamburger'),
+                        Text(
+                          'Hamburger',
+                          style: titleFontStyle,
+                        ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text('Best Food in 2021'),
-                            Text('Best Food in 2021'),
+                            Text(
+                              'Best Food in 2021',
+                              style: subtitleFontStyle,
+                            ),
+                            Row(
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      qty--;
+                                    });
+                                  },
+                                  child: Container(
+                                    width: 25,
+                                    height: 25,
+                                    padding: EdgeInsets.all(7),
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey.shade300,
+                                      shape: BoxShape.circle,
+                                    ),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: AssetImage(
+                                                'assets/icon/minus.png')),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 12),
+                                Text(
+                                  qty.toString(),
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                SizedBox(width: 12),
+                                GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      qty++;
+                                    });
+                                  },
+                                  child: Container(
+                                    width: 25,
+                                    height: 25,
+                                    padding: EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                      color: mainColor,
+                                      shape: BoxShape.circle,
+                                    ),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: AssetImage(
+                                                'assets/icon/add.png')),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ],
                         ),
-                        Text('IDR 30000'),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          'IDR 30000',
+                          style: priceFontStyle,
+                        ),
                       ],
                     ),
                   ),
--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 const double defaultMargin = 24;
 
 Color mainColor = Color(0xff11AC6A);
 Color greyColor = Color(0xffA4A4A4);
 Color lightGreyColor = Color(0xffF6F7FB);
+
+TextStyle titleFontStyle = GoogleFonts.poppins().copyWith(
+  color: Colors.black,
+  fontSize: 14,
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle subtitleFontStyle = GoogleFonts.poppins().copyWith(
+  color: greyColor,
+  fontSize: 12,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle priceFontStyle = GoogleFonts.poppins().copyWith(
+  color: mainColor,
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
